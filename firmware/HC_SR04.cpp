c347d04981681bbc78f9625feddc0a653428b811
Simon Monk <Si@Simons-Mac-Pro.local>
2015-01-27 09:40:05 +0000

    rearranged

--- firmware/HC_SR04.cpp
+++ firmware/HC_SR04.cpp
@@ -2,13 +2,34 @@
  
 HC_SR04::HC_SR04(int trigPin, int echoPin){
   _trigPin = trigPin;
-  _echPin = echoPin;
+  _echoPin = echoPin;
+  pinMode(_trigPin, OUTPUT);
+  pinMode(_echoPin, INPUT);
 }
-double HC_SR04::getDistanceCM(){
 
+double HC_SR04::getDistanceCM(){
+  sendTriggerPulse(_trigPin);
+  waitForEcho(_echoPin, HIGH, 100);
+  long startTime = millis();
+  waitForEcho(_echoPin, LOW, 100);
+  long endTime = millis();
+  long duration = endTime - startTime;
+  double distance = duration * 1000.0 / 29.0 / 2.0;
+  return distance;
 }
 
 double HC_SR04::getDistanceInch(){
   return (getDistanceCM() * 2.5);
+}
+
+void HC_SR04::sendTriggerPulse(int pin){
+    digitalWrite(pin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(pin, LOW);
+}
+
+void HC_SR04::waitForEcho(int pin, int value, long timeout){
+    long giveupTime = millis() + timeout;
+    while (digitalRead(pin) != value && millis() < giveupTime) {}
 }
 
--- firmware/HC_SR04.h
+++ firmware/HC_SR04.h
@@ -9,6 +9,13 @@ public:
   HC_SR04(int trigPin, int echoPin);  
   double getDistanceCM();  
   double getDistanceInch();
-}
+  
+private:
+  int _echoPin;
+  int _trigPin;
+  
+  void sendTriggerPulse(int pin);
+  void waitForEcho(int pin, int value, long timeout);
+};
  
 #endif  
