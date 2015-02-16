0ac7d8fa8b214e62d804c1f0c5353da297d7b4b1
Simon Monk <srmonk@gmail.com>
2015-02-16 09:19:24 +0000

    Update HC_SR04.h

--- firmware/HC_SR04.h
+++ firmware/HC_SR04.h
@@ -6,7 +6,7 @@
 class HC_SR04
 {
 public: 
-  HC_SR04(int trigPin, int echoPin, double minCM=10.0, double maxCM=250.0); 
+  HC_SR04(int trigPin, int echoPin, double minCM, double maxCM); 
   double getDistanceCM();  
   double getDistanceInch();
   
